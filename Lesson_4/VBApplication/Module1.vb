5280dc21b5f7bcb758072c2978b60c22276fcb3b
Dankhra sandip <39943347+SandipDankhra@users.noreply.github.com>
2020-05-02 14:19:00 +0530

    lesson_4 has added

---
+++ Lesson_4/VBApplication/Module1.vb
@@ -0,0 +1,10 @@
+﻿Imports MathUtil
+Module Module1
+    Sub Main()
+        Dim r As New RandomNumber() With {
+        .MinValue = 0,
+        .MaxValue = 500
+        }
+        Console.WriteLine(r.[Get]())
+    End Sub
+End Module
